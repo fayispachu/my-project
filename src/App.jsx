8f10d2a6ba591c66b588ef3bf7ef65579d31416f
Fayis <fayizpachu217@gmail.com>
2024-11-12 12:15:28 +0530

    fix favourite

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import BookList from "./component/BookList";
+// import BookList from "./component/BookList";
 
 import Preview from "./component/Preview";
 import Home from "./pages/Home";
@@ -12,11 +12,10 @@ function App() {
     <>
       <BrowserRouter>
         <Header />
+
         <Routes>
           <Route path="/" element={<Home />} />
-
           <Route path="/preview" element={<Preview />} />
-          <Route path="/" element={<BookList />} />
           <Route path="/favorites" element={<Favorites />} />
         </Routes>
       </BrowserRouter>
